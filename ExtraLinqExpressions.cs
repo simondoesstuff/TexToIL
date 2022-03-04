e9e7048b0b68675aada615b0b5dc5d5cecd51ce6
simondoesstuff <sw.simonwalker@gmail.com>
2022-03-03 22:09:58 -0700

    utility class for extra Linq Expressions

---
+++ ExtraLinqExpressions.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Linq.Expressions;
+
+namespace LatexProcessing2;
+
+public static class ExtraLinqExpressions
+{
+    public static Expression Sqrt(Expression e)
+    {
+        var sqrtMethod = typeof(Math).GetMethod("Sqrt", new[] { typeof(double) });
+        return Expression.Call(sqrtMethod, e);
+    }
+}
